f53d40fec9c5857ac0ac416112900441947606ea
olovm <olov.mckie@ub.uu.se>
2019-04-05 09:18:47 +0000

    remove module, update tests for place in fedora

--- src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module se.uu.ub.cora.alvin.fitnesse {
-	requires org.apache.commons.text;
-	requires se.uu.ub.cora.bookkeeper;
-	requires transitive se.uu.ub.cora.alvin.mixedstorage;
-
-	exports se.uu.ub.cora.alvin.fitnesse;
-}
